cc8212c5e6f208c99dfd6d0b407f6aefe9dd65cc
Nergan <mak.kriuchkov@yandex.ru>
2026-09-25 19:25:24 +0700

    fix 4

--- src/main/kotlin/com/patchoulibutton/mod/mixin/GuiBookLandingMixin.kt
+++ src/main/kotlin/com/patchoulibutton/mod/mixin/GuiBookLandingMixin.kt
@@ -5,41 +5,35 @@ import com.patchoulibutton.mod.client.CompendiumIcons
 import com.patchoulibutton.mod.client.ownsCompendium
 import net.minecraft.client.resources.language.I18n
 import org.spongepowered.asm.mixin.Mixin
-import org.spongepowered.asm.mixin.Shadow
 import org.spongepowered.asm.mixin.injection.At
 import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.Redirect
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 import vazkii.patchouli.client.book.gui.GuiBook
 import vazkii.patchouli.client.book.gui.GuiBookLanding
-import vazkii.patchouli.common.book.Book
 
 @Mixin(GuiBookLanding::class)
 abstract class GuiBookLandingMixin {
-    @Shadow
-    lateinit var book: Book
-
-    @field:Shadow
-    @JvmField
-    var spread: Int = 0
-
-    @field:Shadow
-    @JvmField
-    var maxSpreads: Int = 0
+    private val pages: GuiBookAccessor
+        get() = this as GuiBookAccessor
 
     @Inject(method = ["init"], at = [At("TAIL")])
     private fun installCompendiumIcons(callback: CallbackInfo) {
+        val book = (this as GuiBook).book
         if (!book.ownsCompendium()) return
-        maxSpreads = CompendiumIcons.pageCount()
-        if (spread >= maxSpreads) spread = maxSpreads - 1
-        CompendiumIcons.install(this as GuiBook, spread)
+        pages.patchouliButtonSetMaxSpreads(CompendiumIcons.pageCount())
+        if (pages.patchouliButtonSpread() >= pages.patchouliButtonMaxSpreads()) {
+            pages.patchouliButtonSetSpread(pages.patchouliButtonMaxSpreads() - 1)
+        }
+        CompendiumIcons.install(this as GuiBook, pages.patchouliButtonSpread())
     }
 
     @Inject(method = ["onPageChanged"], at = [At("HEAD")], cancellable = true)
     private fun turnCompendiumPage(callback: CallbackInfo) {
+        val book = (this as GuiBook).book
         if (!book.ownsCompendium()) return
-        maxSpreads = CompendiumIcons.pageCount()
-        CompendiumIcons.install(this as GuiBook, spread)
+        pages.patchouliButtonSetMaxSpreads(CompendiumIcons.pageCount())
+        CompendiumIcons.install(this as GuiBook, pages.patchouliButtonSpread())
         callback.cancel()
     }
 
@@ -51,6 +45,7 @@ abstract class GuiBookLandingMixin {
         ),
     )
     private fun compendiumHeader(key: String): String {
+        val book = (this as GuiBook).book
         if (book.id == CompendiumBook.ID && key == "patchouli.gui.lexicon.categories") {
             return I18n.get("patchoulibutton.book.guides")
         }
